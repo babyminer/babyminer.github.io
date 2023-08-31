23fc0efb242ec9818cbd1de102dc893e932e2a92
DeFiDev <defidev2021@gmail.com>
2023-08-31 16:20:08 -0700

    update

---
+++ precache-manifest.67d8f3d7aed30d645f35bdb43c280c67.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "02e84eeaecbca1883906",
+    "url": "/static/css/main.2effbc58.chunk.css"
+  },
+  {
+    "revision": "02e84eeaecbca1883906",
+    "url": "/static/js/main.02e84eea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "ccb1f20afb30d7dfd7c9",
+    "url": "/static/css/2.ae0e508a.chunk.css"
+  },
+  {
+    "revision": "ccb1f20afb30d7dfd7c9",
+    "url": "/static/js/2.ccb1f20a.chunk.js"
+  },
+  {
+    "revision": "db92def1500c188c3dc7d5c703217b9c",
+    "url": "/static/media/totalMinerWrap.db92def1.png"
+  },
+  {
+    "revision": "22b47bee29707e5840295a2353f1c5f8",
+    "url": "/static/media/textLogo.22b47bee.png"
+  },
+  {
+    "revision": "091d3bd2e4f08f69a14b6088cadc219e",
+    "url": "/static/media/walletBalanceWrap.091d3bd2.png"
+  },
+  {
+    "revision": "467f99f7e139aea68911c8c60b8b2695",
+    "url": "/static/media/miner.467f99f7.png"
+  },
+  {
+    "revision": "21dad5b1190aba873318dba9e8857829",
+    "url": "/static/media/mineWrap.21dad5b1.png"
+  },
+  {
+    "revision": "743a68a2fb865c901b0edbdea5a0ada3",
+    "url": "/static/media/mobileMineWrap.743a68a2.png"
+  },
+  {
+    "revision": "1033ab143991ee2c2121aa00a8b2c07b",
+    "url": "/static/media/referralWarp.1033ab14.png"
+  },
+  {
+    "revision": "1cc9265ad3c1dbad2f52426a60325dbd",
+    "url": "/static/media/fundWrap.1cc9265a.png"
+  },
+  {
+    "revision": "3c0eb968a09e916b49b5450467c7119d",
+    "url": "/static/media/bscscan.3c0eb968.svg"
+  },
+  {
+    "revision": "21dad5b1190aba873318dba9e8857829",
+    "url": "/static/media/dividendsWrap.21dad5b1.png"
+  },
+  {
+    "revision": "743a68a2fb865c901b0edbdea5a0ada3",
+    "url": "/static/media/dividendsWrapMobile.743a68a2.png"
+  },
+  {
+    "revision": "b53147133e3bc1272596ac2966778b81",
+    "url": "/static/media/stakeUnstakeWrap.b5314713.png"
+  },
+  {
+    "revision": "3df249b0adaab75906532f5e7d2734d9",
+    "url": "/static/media/claimWrap.3df249b0.png"
+  },
+  {
+    "revision": "c2770b81cd99df00000ea10184fb5c44",
+    "url": "/static/media/box.c2770b81.png"
+  },
+  {
+    "revision": "c7f98c223ecca74276c5ca7e48d79f4a",
+    "url": "/static/media/mobileBg.c7f98c22.jpg"
+  },
+  {
+    "revision": "ccb3227d0b124d70c9f539c684d543d2",
+    "url": "/static/media/bg.ccb3227d.jpg"
+  },
+  {
+    "revision": "4ba29b103ba975b46f01db52f54e2649",
+    "url": "/index.html"
+  }
+];
